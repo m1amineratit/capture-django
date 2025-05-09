4c6f34e368185ff4ed73ecfe40f63f0b0972836d
Hcode7 <ratitamine3@gmail.com>
2025-05-10 00:23:09 +0100

    deploy project

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Image
 
-# Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'img', 'created_at')  # Make sure 'created_at' is included here
+    search_fields = ('img',)
+
+admin.site.register(Image, ImageAdmin)
