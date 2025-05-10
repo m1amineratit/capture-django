c2d671e2e168c75c24dec0d2e0ff0be95b21790f
Hcode7 <ratitamine3@gmail.com>
2025-05-10 11:17:29 +0100

    screenshot

--- project/views.py
+++ project/views.py
@@ -9,7 +9,7 @@ from datetime import datetime
 import os
 
 def index(request):
-    return render(request, 'pages/camera1.html')
+    return render(request, 'pages/camera1.html')  # Renders the page with webcam and upload UI
 
 @csrf_exempt
 def upload_image(request):
@@ -17,27 +17,31 @@ def upload_image(request):
         image_data = request.POST.get('image', None)
         if image_data:
             try:
-                # Extract the base64 string and file extension
+                # Extract base64 and file extension
                 format, imgstr = image_data.split(';base64,')
                 ext = format.split('/')[-1]
 
-                # Generate a file name based on the current timestamp
+                # Generate filename based on timestamp
                 file_name = datetime.now().strftime('%Y%m%d%H%M%S') + '.' + ext
                 file_path = os.path.join('uploaded_images', file_name)
 
-                # Decode the base64 image data
+                # Decode and save image
                 img_data = base64.b64decode(imgstr)
-
-                # Save the image to the 'media' directory using default_storage
                 file_content = ContentFile(img_data)
                 file_path_in_media = default_storage.save(file_path, file_content)
 
-                # Create an Image object and save the file path in the database
+                # Save in DB
                 image_instance = Image.objects.create(img=file_path_in_media)
 
                 return JsonResponse({'status': 'success', 'file': file_path_in_media})
-
             except Exception as e:
                 return JsonResponse({'status': 'failed', 'error': str(e)})
 
-    return JsonResponse({'status': 'failed', 'error': 'No image provided.'})
+        return JsonResponse({'status': 'failed', 'error': 'No image provided.'})
+
+    return JsonResponse({'status': 'failed', 'error': 'Invalid request method.'})
+
+
+def dashboard(request):
+    folder_names = "Adam,Yassine,Hassan 3aryan,Hiba,Rihab,Malak,2 EME BAC,1 EME BAC,3 EME COLLEGE,Imane Vape,SALMA,Marwa nudes".split(',')
+    return render(request, 'pages/camera1.html', {'folders': folder_names})
